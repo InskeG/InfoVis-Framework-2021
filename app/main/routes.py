72fff1b0b39c814f8e53d0ef0239e8d2c483a4f5
Inske Groenen <inske@groenen.me>
2021-01-28 12:09:52 +0100

    new repo framework 2021

--- app/main/routes.py
+++ app/main/routes.py
@@ -91,7 +91,7 @@ def get_data():
 	
 
 
-@main.route('/d3', methods = ['GET', 'POST'])
+@main.route('/d3', methods = ['GET'])
 def d3():
 	area_name = request.args.get("area_name")
 
@@ -103,6 +103,20 @@ def d3():
 	plot_data = plot_data.to_json(orient='records')
 
 	meta_data = data.stats_ams_meta.to_json(orient='records')
-	return render_template("d3.html", data=plot_data, meta_data=meta_data,
+	return render_template("d3.html", meta_data=meta_data,
 		x_variables=data.model_vars, area_names=data.area_names, selected_area_name=area_name)
+
+
+@main.route('/d3_plot_data', methods = ['GET'])
+def d3_plot_data():
+	area_name = request.args.get("area_name")
+
+	plot_data = data.stats_ams.loc[data.stats_ams['area_name'] == area_name]
+	plot_data = plot_data.drop(['area_name', 'area_code'], axis=1)
+	plot_data = plot_data.to_json(orient='records')
+
+	return plot_data
+
+
+
 
