eecfa36c733f8da2cee18a70dbd86898d5091e1e
Abe <abe_vos@msn.com>
2021-02-16 15:48:18 +0100

    Tweaks

--- front_end/src/views/IndexPage.vue
+++ front_end/src/views/IndexPage.vue
@@ -1,28 +1,5 @@
 <template>
 	<vs-root>
-		<vs-row type="flex" vs-justify="center" vs-align="center" vs-w="12">
-			<vs-card class="cardx">
-				<div class="d-flex align-items-center dropdownbtn-alignment">
-					<vs-dropdown vs-trigger-click>
-						<vs-button class="btn-alignment" type="filled" icon="expand_more">Pick a style!</vs-button>
-						<vs-dropdown-menu>
-							<vs-dropdown-item @click="get_info('kaas')">
-								Kaas
-							</vs-dropdown-item>
-							<vs-dropdown-item @click="get_info('pindakaas')">
-								Pindakaas
-							</vs-dropdown-item>
-							<vs-dropdown-item @click="get_info('worst')">
-								Worst
-							</vs-dropdown-item>
-							<vs-dropdown-item @click="get_info('hagelslag')">
-								Hagelslag
-							</vs-dropdown-item>
-						</vs-dropdown-menu>
-					</vs-dropdown>
-				</div>
-			</vs-card>
-		</vs-row>
 
 		<vs-row type="flex" vs-justify="center" vs-align="center">
 			<vs-card class="cardx" v-if="data.fetched" fixedHeight vs-w="12">
@@ -48,6 +25,8 @@
 					<div slot="header"><h3>Meer statistieken</h3></div>
 					<div>
 						alskjhd lksjh sfajkh asjfh lkasfh
+						Kijk deze krab nou. Zie hem krabben alsof er geen morgen is.
+						Een ware inspiratie voor iedere liefhebber van geleedpotigen.
 					</div>
 				</vs-card>
 			</vs-col>
@@ -60,6 +39,31 @@
 					</div>
 				</vs-card>
 			</vs-col>
+		</vs-row>
+
+		<vs-row type="flex" vs-justify="center" vs-align="center" vs-w="12">
+			<vs-card class="cardx">
+				<div slot="header"><h3>Pick a style!</h3></div>
+				<div class="d-flex align-items-center dropdownbtn-alignment">
+					<vs-dropdown vs-trigger-click>
+						<vs-button class="btn-alignment" type="filled" icon="expand_more">Pick a style!</vs-button>
+						<vs-dropdown-menu>
+							<vs-dropdown-item @click="get_info('kaas')">
+								Kaas
+							</vs-dropdown-item>
+							<vs-dropdown-item @click="get_info('pindakaas')">
+								Pindakaas
+							</vs-dropdown-item>
+							<vs-dropdown-item @click="get_info('worst')">
+								Worst
+							</vs-dropdown-item>
+							<vs-dropdown-item @click="get_info('hagelslag')">
+								Hagelslag
+							</vs-dropdown-item>
+						</vs-dropdown-menu>
+					</vs-dropdown>
+				</div>
+			</vs-card>
 		</vs-row>
 	</vs-root>
 </template>
