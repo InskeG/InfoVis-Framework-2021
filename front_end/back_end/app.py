9511665451e0682282c973699664899947365b18
Jardenna Mohazzab <jardenna@Jardennas-MacBook-Pro.local>
2021-02-16 16:21:31 +0100

    working retrieval of info

--- front_end/back_end/app.py
+++ front_end/back_end/app.py
@@ -3,7 +3,7 @@ from retrieve_info import retrieve_info
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-# for example: http://127.0.0.1:5000/info/Post-impressionism/1943/vincent+van+gogh
+# for example: http://127.0.0.1:5000/info/impressionism/1954/vincent%20van%20gogh
 @app.route('/info/<genre>/<year>/<artist>', methods=['GET'])
 def index(genre, year, artist):
     json_info = jsonify(retrieve_info(genre, year, artist))
--- front_end/back_end/retrieve_info.py
+++ front_end/back_end/retrieve_info.py
@@ -1,16 +1,13 @@
+from data import *
+import pandas as pd  
 import wikipedia
 
 
-#for example: http://127.0.0.1:5000/info/Post-impressionism/1943/vincent+van+gogh
+#for example: http://127.0.0.1:5000/info/impressionism/1954/vincent%20van%20gogh
+# year is nodig om genre te bepalen naderhand want die staan niet in dataset
 def retrieve_info(genre, year, artist):
-    year = float(year)
-    ranges = float(1)
     dictionary = {}
-    # genre = "Post-impressionism"
-    # year = float(1943)
-    # artist = 'vincent van gogh'
 
-    print('hi')
     w = wikipedia.page(genre)
     dictionary['genre'] = w.title
 
@@ -21,20 +18,16 @@ def retrieve_info(genre, year, artist):
     other = model_data.loc[model_data['artist_full_name'] == artist]
 
     if len(other) <= 5:
-        print(other['artwork_name'])
+        dictionary['other_pieces_by_artist'] = pd.Series.tolist(other['artwork_name'])
     else:
-        print(other[:5]['artwork_name'])
-
-    dictionary['other_pieces_by_artist'] = other
-
-    # print('artworks in same time period:')
-    # type(model_data['creation_year'][0])
+        dictionary['other_pieces_by_artist'] = pd.Series.tolist(other[:5]['artwork_name'])
 
     return dictionary
 
 
-# print(retrieve_info('ho', 1934, 'jio'))
-
+# print('artworks in same time period:')
 # other art pieces created in same year
-# year_df = model_data.loc[model_data['creation_year'].astype('float') < year+ranges]
-# year_df = year_df.loc[year_df['creation_year'].astype('float') > year-ranges]
+    year_df = model_data.loc[model_data['creation_year'].astype('float') < year+ranges]
+    year_df = year_df.loc[year_df['creation_year'].astype('float') > year-ranges]
+
+    dictionary['same_year'] = pd.Series.tolist(year_df)
