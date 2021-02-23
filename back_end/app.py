8272a3f9e7e2e57a8bf8ef2b9b95c6bddeab5872
Jardenna Mohazzab <jardenna@Jardennas-MacBook-Pro.local>
2021-02-23 21:25:08 +0100

    pie chart

--- back_end/app.py
+++ back_end/app.py
@@ -33,21 +33,38 @@ def detect_colors(image):
     vecs, dist = scipy.cluster.vq.vq(array, codes)
     counts, bins = scipy.histogram(vecs, len(codes))
 
+    total = np.sum(np.array(counts))
+    percentages = (counts/total)
+    # print(percentage)
     # Find the most prominent color.
-    maxi = counts.argsort()[-nr_color:][::-1]
+    # maxi = counts.argsort()[-nr_color:][::-1]
+    # print(maxi)
     # index_max = scipy.argmax(counts)
     # peak = codes[index_max]
-    peaks = [codes[i] for i in maxi]
+    # peaks = [codes[i] for i in maxi]
+
+    color_list = []
+    for code in codes:
+        color_list.append(f"#{binascii.hexlify(bytearray(int(c) for c in code)).decode('ascii')}")
+
+    # tups = list(zip(color_list, percentages))
+
+    dom_color = codes[scipy.argmax(counts)]
+    dom_color = f"#{binascii.hexlify(bytearray(int(c) for c in dom_color)).decode('ascii')}"
+
+    colors = color_list
+
+    return list(colors), list(percentages), dom_color
+
 
     # Convert the color to hex representation.
     # color = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
-    colors = []
-    for peak in peaks:
-        colors.append(f"#{binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')}")
+    # colors = []
+    # for peak in peaks:
+    #     colors.append(f"#{binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')}")
         # colors.append(binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii'))
 
     # return f"#{color}"
-    return colors
 
 
 @app.route('/info/<genre>/<year>', methods=['GET'])
@@ -57,7 +74,7 @@ def index(genre, year):
 
     # Load a placeholder image.
     # TODO: Obtain a generated image here.
-    path = os.path.join(os.path.dirname(__file__), "img2.jpg")
+    path = os.path.join(os.path.dirname(__file__), "img1.jpg")
     image = Image.open(path)
 
     # Encode the image for the response.
@@ -67,9 +84,10 @@ def index(genre, year):
     info['image'] = f"data:image/png;base64, {encoded_img}"
 
     # Get the primary color of the image.
-    colors = detect_colors(image)
-    info['dominant_colors'] = colors
-    info['color'] = colors[0]
+    colors, percentages, dom_color = detect_colors(image)
+    info['colors'] = colors
+    info['percentages'] = percentages
+    info['dom_color'] = dom_color
 
 
     json_info = jsonify(info)
