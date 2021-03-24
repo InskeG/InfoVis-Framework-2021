7c4795c77283851f157f0b3843b4243d21625c4d
Sjors Peerdeman <sjorspeerdeman@gmail.com>
2021-03-24 11:47:46 +0100

    Alles op homepage (temp help en footer), hovers gefixt bij kiezen artiesten

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,13 +10,13 @@ from app import models, data
 from . import main
 
 
-@main.route('/', methods=['GET'])
-def index():
-    home_data = data.home_data
-    artists = data.top_artists
-    song_data = dict(zip(artists, home_data))
+#@main.route('/', methods=['GET'])
+#def index():
+    #home_data = data.home_data
+    #artists = data.top_artists
+    #song_data = dict(zip(artists, home_data))
 
-    return render_template("home.html", song_data=song_data, artists=artists)
+    #return render_template("home.html", song_data=song_data, artists=artists)
 
 # @main.route('/d3', methods = ['GET'])
 # def d3():
@@ -35,7 +35,7 @@ def index():
 #     print(mj_json)
 #     return jsonify(mj_json)
 
-@main.route('/metrics', methods=['GET'])
+@main.route('/', methods=['GET'])
 def metrics():
     song_data = data.song_data
     keys = data.filter_columns
@@ -62,7 +62,7 @@ def metrics():
     barchart_data = barchart_data.head(10).groupby("artists")
     barchart_data = {x[0] : x[1].filter(items=keys).to_dict("records") for x in barchart_data}
     # print(barchart_data)
-    return render_template("metrics.html", heatmap_data=heatmap_data,
+    return render_template("home.html", heatmap_data=heatmap_data,
                            heatmap_colors=colors, song_data=barchart_data,
                            artists=artists, keys=keysAxis, popularity=popularity,
                            all_artists=all_artists, max_temp_art=max_temp_art,
@@ -75,6 +75,7 @@ def metrics_data():
     heatmap_data = data.heatmap_data
     artist = request.args.get("artist")
     keys = data.filter_columns
+    keysAxis = keys[:9]
     if artist not in data.filtered_artists:
         print("Artist not found")
         return {}
@@ -85,13 +86,13 @@ def metrics_data():
     artist_data = heatmap_data.query("artists == @artist")
     popularity = {artist: artist_data["popularity"].to_list()}
     #print(popularity)
-    heatmap_data = artist_data.filter(items=keys + ["artists"])
+    heatmap_data = artist_data.filter(items=keysAxis + ["artists"])
     heatmap_data = pd.melt(heatmap_data, id_vars=["artists"], var_name=["characteristic"])
     heatmap_data = heatmap_data.to_dict(orient="records")
 
     return {"heatmap" : heatmap_data, "barchart": barchart_data, "popularity": popularity}
 
 
-@main.route('/popularity', methods=['GET'])
-def popularity():
-    return render_template("popularity.html")
+#@main.route('/popularity', methods=['GET'])
+#def popularity():
+    #return render_template("popularity.html")
